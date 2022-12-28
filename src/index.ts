0b164e1706ac508c31f6a939111bd96014bd0af6
acou12 <51276518+acou12@users.noreply.github.com>
2022-12-28 12:30:33 -0500

    Completely rewrite for full image matching

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,103 @@
 const canvas = document.querySelector("canvas")!;
-canvas.width = 200;
-canvas.height = 400;
+canvas.width = 800;
+canvas.height = 800;
 const c = canvas.getContext("2d")!;
 
+const MUTATION_RATE = 0.2;
+
 type Pattern = {
-  colorIndex: number;
-  patternIndex: number;
+  color: number;
+  data: ImageData;
 };
 
-type Banner = Pattern[];
+class Banner {
+  constructor(public patterns: Pattern[] = []) {}
 
-const patterns: ImageData[] = [];
+  copy() {
+    return new Banner(this.patterns.map((pattern) => ({ ...pattern })));
+  }
+
+  data() {
+    const canvas = document.createElement("canvas");
+    canvas.width = PATTERN_WIDTH;
+    canvas.height = PATTERN_HEIGHT;
+    const context = canvas.getContext("2d")!;
+    this.patterns.forEach((pattern) => {
+      context.drawImage(imageFromData(pattern.data, pattern.color), 0, 0);
+    });
+    return context.getImageData(0, 0, PATTERN_WIDTH, PATTERN_HEIGHT);
+  }
+
+  cost(data: ImageData) {
+    const theirData = data.data;
+    let cost = 0;
+    for (let i = 0; i < theirData.length / 4; i++) {
+      let bannerIndex = this.patterns.length - 1;
+      while (
+        bannerIndex >= 0 &&
+        this.patterns[bannerIndex].data.data[i * 4 + 3] === 0
+      ) {
+        bannerIndex--;
+      }
+      if (!(bannerIndex >= 0))
+        for (let j = 0; j < 3; j++) {
+          cost += Math.pow(256 - theirData[i * 4 + j], 2);
+        }
+      else
+        for (let j = 0; j < 3; j++) {
+          cost += Math.pow(
+            (this.patterns[bannerIndex].data.data[i * 4 + j] *
+              ((this.patterns[bannerIndex].color >> ((2 - j) * 8)) & 0xff)) /
+              256 -
+              theirData[i * 4 + j],
+            2
+          );
+        }
+    }
+    return cost;
+  }
+
+  mutated() {
+    const newBanner = this.copy();
+    const mutations = [
+      () => {
+        // insert
+        const index = randomInteger(0, newBanner.patterns.length);
+        newBanner.patterns = [
+          ...newBanner.patterns.slice(0, index),
+          randomPattern(),
+          ...newBanner.patterns.slice(index),
+        ];
+      },
+      () => {
+        if (newBanner.patterns.length <= 1) return;
+        // delete
+        newBanner.patterns.splice(
+          randomInteger(0, newBanner.patterns.length),
+          1
+        );
+      },
+      () => {
+        // colorshift
+        const randomPattern = randomElement(newBanner.patterns);
+        randomPattern.color = randomElement(colors);
+      },
+      () => {
+        // patternshift
+        const randomPattern = randomElement(newBanner.patterns);
+        randomPattern.data = randomElement(patternDatas);
+      },
+    ];
+
+    do {
+      randomElement(mutations)();
+    } while (Math.random() < MUTATION_RATE);
+
+    return newBanner;
+  }
+}
+
+const patternDatas: ImageData[] = [];
 const colors = [
   0x664c33, 0x191919, 0x4c4c4c, 0x999999, 0xffffff, 0xf27fa5, 0xb24cd8,
   0x7f3fb2, 0x334cb2, 0x4c7f99, 0x6699d8, 0x667f33, 0x7fcc19, 0xe5e533,
@@ -20,14 +107,14 @@ const colors = [
 const image = new Image(100, 320);
 image.src = "/images/banners.png";
 
-const reference = new Image(20, 40);
+const reference = new Image(800, 800);
 reference.src = "/images/reference.png";
-let referenceData: ImageData;
+let referenceDatas: ImageData[] = [];
 
 const PATTERN_WIDTH = 20;
 const PATTERN_HEIGHT = 40;
 
-const banners: Banner[] = [];
+const banners: Banner[][] = [];
 
 image.onload = () => {
   reference.onload = () => {
@@ -40,48 +127,65 @@ image.onload = () => {
           x + PATTERN_WIDTH,
           y + PATTERN_HEIGHT
         );
-        // const patternCanvas = document.createElement("canvas");
-        // patternCanvas.width = PATTERN_WIDTH;
-        // patternCanvas.height = PATTERN_HEIGHT;
-        // const patternCanvasC = patternCanvas.getContext("2d")!;
-        // patternCanvasC.putImageData(
-        //   tinted(pattern, Math.floor(Math.random() * 0xffffff)),
-        //   0,
-        //   0
-        // );
-        patterns.push(pattern);
+        patternDatas.push(pattern);
       }
     }
 
-    c.clearRect(0, 0, canvas.width, canvas.height);
-
-    // for (let x = 0; x < canvas.width; x += PATTERN_WIDTH) {
-    //   for (let y = 0; y < canvas.height; y += PATTERN_HEIGHT) {
-    //     drawBanner(randomBanner(), x, y);
-    //   }
-    // }
-
     c.drawImage(reference, 0, 0);
-    referenceData = c.getImageData(0, 0, 20, 40);
-
-    // let bestBanner: Banner | undefined = undefined;
-    // let lowestFitness: number = Infinity;
-
-    // for (let i = 0; i < 100; i++) {
-    //   const banner = randomBanner();
-    //   const bannerFitness = fitness(banner);
-    //   if (bannerFitness < lowestFitness) {
-    //     lowestFitness = bannerFitness;
-    //     bestBanner = banner;
-    //   }
-    // }
-
-    // drawBanner(bestBanner!, 0, 0);
-    // console.log(lowestFitness);
-    for (let i = 0; i < 100; i++) {
-      banners.push(randomBanner());
+    for (let y = 0; y < canvas.height; y += PATTERN_HEIGHT) {
+      for (let x = 0; x < canvas.width; x += PATTERN_WIDTH) {
+        const localData = c.getImageData(x, y, PATTERN_WIDTH, PATTERN_HEIGHT);
+        referenceDatas.push(localData);
+        const banner = randomBanner();
+        banners.push(
+          Array(50)
+            .fill(0)
+            .map(() => banner.copy())
+        );
+      }
     }
-    evolutionStep(banners);
+
+    console.log("Begin...");
+
+    const NUM_X_BANNERS = canvas.width / PATTERN_WIDTH;
+    const NUM_Y_BANNERS = canvas.height / PATTERN_HEIGHT;
+
+    let n = 0;
+
+    const recursiveImprove = () => {
+      console.log(n++);
+      for (let y = 0; y < NUM_Y_BANNERS; y++) {
+        for (let x = 0; x < NUM_X_BANNERS; x++) {
+          const index = x + y * NUM_X_BANNERS;
+          const bannerSet = banners[index];
+          const data = referenceDatas[index];
+          let mutated = bannerSet
+            .map((banner) => banner.mutated())
+            .map((banner) => ({
+              banner,
+              cost: banner.cost(data),
+            }));
+          mutated.sort((a, b) => a.cost - b.cost);
+          let mutatedBanners = mutated.map((m) => m.banner);
+          const topHalf = mutatedBanners.slice(0, mutatedBanners.length / 2);
+          mutatedBanners = topHalf
+            .map((banner) => banner.copy())
+            .concat(topHalf.map((banner) => banner.copy()));
+
+          banners[index] = mutatedBanners;
+
+          if (n % 30 === 0)
+            c.putImageData(
+              mutatedBanners[0].data(),
+              x * PATTERN_WIDTH,
+              y * PATTERN_HEIGHT
+            );
+        }
+      }
+      setTimeout(recursiveImprove, 10);
+    };
+
+    setTimeout(recursiveImprove, 1000);
   };
 };
 
@@ -96,129 +200,62 @@ const tinted = (data: ImageData, color: number) => {
   return result;
 };
 
-const drawBanner = (banner: Banner, x: number, y: number) => {
-  for (let pattern of banner) {
-    const color = colors[pattern.colorIndex];
-    const patternData = patterns[pattern.patternIndex];
-    const patternCanvas = document.createElement("canvas");
-    patternCanvas.width = PATTERN_WIDTH;
-    patternCanvas.height = PATTERN_HEIGHT;
-    const patternCanvasC = patternCanvas.getContext("2d")!;
-    patternCanvasC.putImageData(tinted(patternData, color), 0, 0);
-    c.drawImage(patternCanvas, x, y);
-  }
+const enumerate = (n: number) => {
+  return [...Array(n).keys()];
+};
+
+const randomInteger = (inclusiveMin: number, exclusiveMax: number) =>
+  inclusiveMin + Math.floor(Math.random() * (exclusiveMax - inclusiveMin));
+
+const randomExp = (lambda: number) => -Math.log(1 - Math.random()) / lambda;
+
+const randomElement = <T>(ts: T[]): T => ts[randomInteger(0, ts.length)];
+
+const randomBanner = () => {
+  const numPatterns = Math.ceil(randomExp(0.2));
+  return new Banner(
+    Array(numPatterns)
+      .fill(0)
+      .map(() => ({
+        color: randomElement(colors),
+        data: randomElement(patternDatas),
+      }))
+      .map((pattern) => ({
+        ...pattern,
+        canvas: imageFromData(pattern.data, pattern.color),
+      }))
+  );
+};
+
+const imageFromData = (data: ImageData, color: number) => {
+  const canvas = document.createElement("canvas");
+  canvas.width = data.width;
+  canvas.height = data.height;
+  const context = canvas.getContext("2d")!;
+  context.putImageData(tinted(data, color), 0, 0);
+  return canvas;
 };
 
 const randomPattern = (): Pattern => {
+  const data = randomElement(patternDatas);
+  const color = randomElement(colors);
   return {
-    colorIndex: Math.floor(Math.random() * colors.length),
-    patternIndex: Math.floor(Math.random() * patterns.length),
+    data,
+    color,
   };
 };
 
-const randomBanner = (): Banner => {
-  const banner: Banner = [];
-  for (let i = 0; i < 10; i++) {
-    banner.push(randomPattern());
-  }
-  return banner;
-};
-
-let numberFitness = 0;
-
-const fitness = (banner: Banner) => {
-  c.clearRect(0, 0, 20, 40);
-  drawBanner(banner, 0, 0);
-  const data = c.getImageData(0, 0, 20, 40);
-  let fitness = 0;
-  for (let i = 0; i < data.data.length; i += 4) {
-    fitness += Math.abs(data.data[i] - referenceData.data[i]);
-    fitness += Math.abs(data.data[i + 1] - referenceData.data[i + 1]);
-    fitness += Math.abs(data.data[i + 2] - referenceData.data[i + 2]);
-    // fitness += Math.abs(data.data[i] - 255 * (i / data.data.length));
-    // fitness += Math.abs(data.data[i + 1]);
-    // fitness += Math.abs(data.data[i + 2]);
-  }
-  return fitness;
-};
-
-const cloneBanner = (banner: Banner) => {
-  const newBanner: Banner = [];
-  for (let pattern of banner) {
-    newBanner.push({
-      colorIndex: pattern.colorIndex,
-      patternIndex: pattern.patternIndex,
-    });
-  }
-  return newBanner;
-};
-
-const MUTATION_RATE = 0.1;
-
-const mutate = (banner: Banner) => {
-  let newBanner = cloneBanner(banner);
-  const mutation = Math.floor(Math.random() * 4);
-  if (mutation == 0)
-    do {
-      // Insert
-      const index = Math.floor(Math.random() * newBanner.length);
-      newBanner = [
-        ...newBanner.slice(0, index),
-        randomPattern(),
-        ...newBanner.slice(index, newBanner.length),
-      ];
-    } while (Math.random() < MUTATION_RATE);
-  if (mutation == 1)
-    do {
-      // Delete
-      const index = Math.floor(Math.random() * newBanner.length);
-      newBanner.splice(index, 1);
-    } while (Math.random() < MUTATION_RATE);
-  if (mutation == 2)
-    do {
-      // Colorshift
-      const index = Math.floor(Math.random() * newBanner.length);
-      if (Math.random() < 0.5) newBanner[index].colorIndex += 1;
-      else newBanner[index].colorIndex -= 1;
-      newBanner[index].colorIndex %= colors.length;
-      if (newBanner[index].colorIndex < 0)
-        newBanner[index].colorIndex += colors.length;
-    } while (Math.random() < MUTATION_RATE);
-  if (mutation == 3)
-    do {
-      // Patternshift
-      const index = Math.floor(Math.random() * newBanner.length);
-      if (Math.random() < 0.5) newBanner[index].patternIndex += 1;
-      else newBanner[index].patternIndex -= 1;
-      newBanner[index].patternIndex %= patterns.length;
-      if (newBanner[index].patternIndex < 0)
-        newBanner[index].patternIndex += patterns.length;
-    } while (Math.random() < MUTATION_RATE);
-  return newBanner;
-};
-
-const drawAll = (banners: Banner[]) => {
-  let i = 0;
-  for (let y = 0; y < canvas.height; y += PATTERN_HEIGHT) {
-    for (let x = 0; x < canvas.width; x += PATTERN_WIDTH) {
-      drawBanner(banners[i++], x, y);
+const expPick = <T>(ts: T[], n: number, l: number) => {
+  let picked: number[] = [];
+  let result: T[] = [];
+  while (result.length < n) {
+    const index = Math.floor(randomExp(l));
+    if (!picked.includes(index) && index < ts.length) {
+      result.push(ts[index]);
+      picked.push(index);
     }
   }
-};
-
-const evolutionStep = (population: Banner[]) => {
-  const fitnesses = population.map((b) => ({
-    banner: b,
-    fitness: fitness(b),
-  }));
-  fitnesses.sort((a, b) => a.fitness - b.fitness);
-  const sortedBanners = fitnesses.map((it) => it.banner);
-  drawAll(sortedBanners);
-  const firstHalf = sortedBanners.slice(0, 50);
-  setTimeout(
-    () => evolutionStep([...firstHalf, ...firstHalf.map((it) => mutate(it))]),
-    1000
-  );
+  return result;
 };
 
 /*
